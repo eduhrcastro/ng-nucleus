58a9bec61df6dbb8f1c01169d441d72d97483552
Eduardo Castro <eduardo@nucleus.eti.br>
2019-03-18 15:57:18 -0300

    Modify to show all erros in console

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 if (!angular.isFunction(moment)) {
   throw new Error('Moment cannot be found by ng-nucleus!')
-} else if (!angular.isFunction(angular)) {
-  throw new Error('Angular cannot be found by ng-nucleus!')
-} else if (!angular.isFunction(validator)) {
+}
+if (!angular.isFunction(validator)) {
   throw new Error('Validator.js cannot be found by ng-nucleus!')
-} else if (!angular.isFunction(BrV)) {
+}
+if (!angular.isFunction(BrV)) {
   throw new Error('Br-validations.js cannot be found by ng-nucleus!')
 }
 
