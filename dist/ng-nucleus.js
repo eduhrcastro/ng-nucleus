2aa79ba0b06e1f578b73a5df6dcc7842eaacc6f8
Eduardo Castro <eduardo@nucleus.eti.br>
2019-03-24 23:32:17 -0300

    merge with branch 'dev'

--- dist/ng-nucleus.js
+++ dist/ng-nucleus.js
@@ -40,6 +40,89 @@ exports.default = _default;
 "use strict";
 
 (function () {
+  angular.module('ngNucleus').factory('Validations', ['$window', function ($window) {
+    return {
+      isCnpj: function isCnpj(value) {
+        return $window.BrV.cnpj.validate(value.toString());
+      },
+      isCpf: function isCpf(value) {
+        return $window.BrV.cpf.validate(value.toString());
+      },
+      isDate: function isDate(value, dateFormat) {
+        if (angular.isUndefined(dateFormat) || dateFormat === '') {
+          return moment(value).isValid();
+        }
+
+        return moment(value, dateFormat).isValid() && value.length === dateFormat.length;
+      },
+      isISODateString: function isISODateString(value) {
+        return /^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}([-+][0-9]{2}:[0-9]{2}|Z)$/.test(value.toString());
+      },
+      isNumber: function isNumber(value) {
+        return !isNaN(value);
+      },
+      isInscricaoMunicipal: function isInscricaoMunicipal(value) {
+        var invalidIM = ['0', '000000000000000', '111111111111111', '222222222222222', '333333333333333', '444444444444444', '555555555555555', '666666666666666', '777777777777777', '888888888888888', '999999999999999'];
+        return !isNaN(value) && !invalidIM.includes(value.toString()) && value.toString().length > 0 && value.toString().length < 16;
+      },
+      isPis: function isPis(value) {
+        return value.toString().length > 10 && value.toString().length < 15 && $window.BrV.pis.validate(value.toString());
+      },
+      isTitulo: function isTitulo(value) {
+        value = value.toString();
+        var dig1 = 0;
+        var dig2 = 0;
+        var tam = value.length;
+        var digitos = value.substr(tam - 2, 2);
+        var estado = value.substr(tam - 4, 2);
+        var titulo = value.substr(0, tam - 2);
+        titulo = '000000000000' + titulo;
+        titulo = titulo.substr(titulo.length - 11, titulo.length - 1);
+        var exce = estado === '01' || estado === '02';
+        dig1 = (titulo.charCodeAt(0) - 48) * 2 + (titulo.charCodeAt(1) - 48) * 9 + (titulo.charCodeAt(2) - 48) * 8 + (titulo.charCodeAt(3) - 48) * 7 + (titulo.charCodeAt(4) - 48) * 6 + (titulo.charCodeAt(5) - 48) * 5 + (titulo.charCodeAt(6) - 48) * 4 + (titulo.charCodeAt(7) - 48) * 3 + (titulo.charCodeAt(8) - 48) * 2;
+        var resto = dig1 % 11;
+
+        if (resto === 0) {
+          if (exce) {
+            dig1 = 1;
+          } else {
+            dig1 = 0;
+          }
+        } else {
+          if (resto === 1) {
+            dig1 = 0;
+          } else {
+            dig1 = 11 - resto;
+          }
+        }
+
+        dig2 = (titulo.charCodeAt(9) - 48) * 4 + (titulo.charCodeAt(10) - 48) * 3 + dig1 * 2;
+        resto = dig2 % 11;
+
+        if (resto === 0) {
+          if (exce) {
+            dig2 = 1;
+          } else {
+            dig2 = 0;
+          }
+        } else {
+          if (resto === 1) {
+            dig2 = 0;
+          } else dig2 = 11 - resto;
+        }
+
+        if (digitos.charCodeAt(0) - 48 === dig1 && digitos.charCodeAt(1) - 48 === dig2) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    };
+  }]);
+})();
+"use strict";
+
+(function () {
   angular.module('ngNucleus').directive('uiCnpj', ['$window', 'Validations', function ($window, Validations) {
     return {
       require: 'ngModel',
@@ -665,89 +748,6 @@ exports.default = _default;
         ngModelCtrl.$parsers.push(function (value) {
           return value.toString().toUpperCase();
         });
-      }
-    };
-  }]);
-})();
-"use strict";
-
-(function () {
-  angular.module('ngNucleus').factory('Validations', ['$window', function ($window) {
-    return {
-      isCnpj: function isCnpj(value) {
-        return $window.BrV.cnpj.validate(value.toString());
-      },
-      isCpf: function isCpf(value) {
-        return $window.BrV.cpf.validate(value.toString());
-      },
-      isDate: function isDate(value, dateFormat) {
-        if (angular.isUndefined(dateFormat) || dateFormat === '') {
-          return moment(value).isValid();
-        }
-
-        return moment(value, dateFormat).isValid() && value.length === dateFormat.length;
-      },
-      isISODateString: function isISODateString(value) {
-        return /^[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]{3}([-+][0-9]{2}:[0-9]{2}|Z)$/.test(value.toString());
-      },
-      isNumber: function isNumber(value) {
-        return !isNaN(value);
-      },
-      isInscricaoMunicipal: function isInscricaoMunicipal(value) {
-        var invalidIM = ['0', '000000000000000', '111111111111111', '222222222222222', '333333333333333', '444444444444444', '555555555555555', '666666666666666', '777777777777777', '888888888888888', '999999999999999'];
-        return !isNaN(value) && !invalidIM.includes(value.toString()) && value.toString().length > 0 && value.toString().length < 16;
-      },
-      isPis: function isPis(value) {
-        return value.toString().length > 10 && value.toString().length < 15 && $window.BrV.pis.validate(value.toString());
-      },
-      isTitulo: function isTitulo(value) {
-        value = value.toString();
-        var dig1 = 0;
-        var dig2 = 0;
-        var tam = value.length;
-        var digitos = value.substr(tam - 2, 2);
-        var estado = value.substr(tam - 4, 2);
-        var titulo = value.substr(0, tam - 2);
-        titulo = '000000000000' + titulo;
-        titulo = titulo.substr(titulo.length - 11, titulo.length - 1);
-        var exce = estado === '01' || estado === '02';
-        dig1 = (titulo.charCodeAt(0) - 48) * 2 + (titulo.charCodeAt(1) - 48) * 9 + (titulo.charCodeAt(2) - 48) * 8 + (titulo.charCodeAt(3) - 48) * 7 + (titulo.charCodeAt(4) - 48) * 6 + (titulo.charCodeAt(5) - 48) * 5 + (titulo.charCodeAt(6) - 48) * 4 + (titulo.charCodeAt(7) - 48) * 3 + (titulo.charCodeAt(8) - 48) * 2;
-        var resto = dig1 % 11;
-
-        if (resto === 0) {
-          if (exce) {
-            dig1 = 1;
-          } else {
-            dig1 = 0;
-          }
-        } else {
-          if (resto === 1) {
-            dig1 = 0;
-          } else {
-            dig1 = 11 - resto;
-          }
-        }
-
-        dig2 = (titulo.charCodeAt(9) - 48) * 4 + (titulo.charCodeAt(10) - 48) * 3 + dig1 * 2;
-        resto = dig2 % 11;
-
-        if (resto === 0) {
-          if (exce) {
-            dig2 = 1;
-          } else {
-            dig2 = 0;
-          }
-        } else {
-          if (resto === 1) {
-            dig2 = 0;
-          } else dig2 = 11 - resto;
-        }
-
-        if (digitos.charCodeAt(0) - 48 === dig1 && digitos.charCodeAt(1) - 48 === dig2) {
-          return true;
-        } else {
-          return false;
-        }
       }
     };
   }]);
